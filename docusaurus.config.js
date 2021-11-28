bdf6d9add5d80892cfefdf05f3f67ddb07bb4499
LiangShuang <liangshuang@sutu.tech>
2021-11-28 10:29:11 +0800

    fix changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,10 +57,6 @@ const darkCodeTheme = require('prism-react-renderer/themes/nightOwl');
           {href: 'https://www.chia.net/blog/', label: 'Blog', position: 'left'},
           {href: 'https://www.chia.net/assets/Chia-Business-Whitepaper-2021-02-09-v1.0.pdf', label: 'Business Whitepaper', position: 'left'},
           {
-            type: 'localeDropdown',
-            position: 'left',
-          },
-          {
             href: 'https://github.com/Chia-Network/chia-docs',
             label: 'Edit Docs on GitHub',
             position: 'right',
