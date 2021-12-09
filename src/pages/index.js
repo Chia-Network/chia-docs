1c21185ce79f0295f63d610de0234f02dda55112
maopengwei <maopengwei@sutu.tech>
2021-12-09 13:32:28 +0800

    add cn about edit index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Translate, {translate} from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -14,11 +15,16 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 {/*    
+        <h1 className="hero__title"> <Translate id="index.hero_title">{siteConfig.title}</Translate></h1>
+        <p className="hero__subtitle"><Translate id="index.hero__subtitle">{siteConfig.tagline}</Translate></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/01introduction/what-is-chia">
+            <Translate id="index.start_learning">
             Start Learning about Chia ⏱️
+            </Translate>
+
           </Link>
         </div>
 */}
