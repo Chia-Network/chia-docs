f2eb06a7b24beabb7b475825aa639966086c1294
roybotbot <roynatian@gmail.com>
2021-12-16 21:40:36 -0500

    formatting and text updates

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -6,68 +6,76 @@ const FeatureList = [
   {
     title: 'Chia Docs',
     Svg: require('../../static/img/chia-docs-icon.svg').default,
+    dest_url: '/docs/01introduction/what-is-chia',
     description: (
       <>
-        Chia is the most decentralized blockchain ever with approximately 350,000 nodes employing the first new Nakamoto Consensus since 2009. The Chialisp on chain programming environment is fully featured while being far more auditable, and secure.
+        Documentation regarding the Chia blockchain, consensus, and protocols.
       </>
     ),
   },
   {
     title: 'Chialisp',
     Svg: require('../../static/img/chialisp-icon.svg').default,
+    dest_url: 'https://chialisp.com',
     description: (
       <>
-      Chia believes that in order to deliver on the promise of the next generation blockchains, we must be regulatory compliant. It is our stated goal to become a public reporting company subject to the Securities Act of 1933. Chia's native currency, XCH or chia, has never been sold by Chia Network for money and we believe it will be regulated as a commodity by the CFTC.
+      Documentation and tutorials for Chialisp, our new Turing complete on-chain smart transaction programming language and development environment.
       </>
+      
     ),
   },
   {
     title: 'Chia Blockchain Wiki',
     Svg: require('../../static/img/chia_leaf_green.svg').default,
+    dest_url: 'https://github.com/Chia-Network/chia-blockchain/wiki',
     description: (
       <>
-      With its innovative Proof Of Space and Time consensus, Chia uses 0.16% of the annual energy consumption of Bitcoin, and 0.36% of Ethereum. Chia is part of the Circular Drive Initiative that reduces e-waste through the secure reuse of storage, promoting the second use of hard drives for Chia farming.
+      Community-run wiki containing instructions and information about best practices regarding setup, hardware, farming, plotting, security, timelords, and more.
       </>
     ),
   },
   {
     title: 'Cultivation Grant Program',
     Svg: require('../../static/img/grant_icon.svg').default,
+    dest_url: 'https://www.chia.net/grants/',
     description: (
       <>
-      With its innovative Proof Of Space and Time consensus, Chia uses 0.16% of the annual energy consumption of Bitcoin, and 0.36% of Ethereum. Chia is part of the Circular Drive Initiative that reduces e-waste through the secure reuse of storage, promoting the second use of hard drives for Chia farming.
+      Our Cultivation Grant Program is designed to enable and expand promising new projects that are being built on the Chia blockchain.
       </>
     ),
   },
   {
     title: 'Chia Network Company FAQ',
     Svg: require('../../static/img/chia_logo.svg').default,
+    dest_url: 'https://www.chia.net/faq/',
     description: (
       <>
-      With its innovative Proof Of Space and Time consensus, Chia uses 0.16% of the annual energy consumption of Bitcoin, and 0.36% of Ethereum. Chia is part of the Circular Drive Initiative that reduces e-waste through the secure reuse of storage, promoting the second use of hard drives for Chia farming.
+      Frequently asked questions about Chia Network, LLC, the company.
       </>
     ),
   },
   {
     title: 'Chia Technical FAQ',
     Svg: require('../../static/img/technical_faq_icon.svg').default,
+    dest_url: 'https://github.com/Chia-Network/chia-blockchain/wiki/FAQ',
     description: (
       <>
-      With its innovative Proof Of Space and Time consensus, Chia uses 0.16% of the annual energy consumption of Bitcoin, and 0.36% of Ethereum. Chia is part of the Circular Drive Initiative that reduces e-waste through the secure reuse of storage, promoting the second use of hard drives for Chia farming.
+      Frequently asked questions regarding the Chia blockchain and its function.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, description, dest_url}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <a href={dest_url}><Svg className={styles.featureSvg} alt={title} /></a>
       </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+      <div className="text--center padding-horiz--md feature-item">
+        <h3><a href={dest_url}>{title}</a></h3>
         <p>{description}</p>
+        
       </div>
     </div>
   );
